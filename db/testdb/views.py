a259d1eda55c9059b08c4aaac4e05d58d603bf93
vamsi0750 <vamsi.krishnaanagisetty@gmail.com>
2019-11-07 03:16:54 +0530

    added to db to web integration

--- db/testdb/views.py
+++ db/testdb/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from testdb.models import Emp
 
 # Create your views here.
+def emp_info(request):
+    employees=Emp.objects.all()
+    return render(request,'hello.html',{"emps":employees})
+
